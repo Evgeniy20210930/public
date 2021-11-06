5bf5bbd4a858f563a3d5fcb3dad3d267f5bd327f
Evgeniy20210930 <primus.b@bk.ru>
2021-11-06 22:30:46 +0500

    обновлено

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-money = float(input('Введите сумму: '))
+money = int(input('Введите сумму: '))
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-tkb = per_cent['ТКБ']*money/100
-skb = per_cent['СКБ']*money/100
-vtb = per_cent['ВТБ']*money/100
-sb = per_cent['СБЕР']*money/100
-deposit = list(map(int, [tkb, skb, vtb, sb]))
+tkb = int(per_cent['ТКБ']*money/100)
+skb = int(per_cent['СКБ']*money/100)
+vtb = int(per_cent['ВТБ']*money/100)
+sb = int(per_cent['СБЕР']*money/100)
+deposit = [tkb, skb, vtb, sb]
 i = max(deposit)
-print ('money =', int(money))
+print('money =', money)
 print('deposit =', deposit)
 print('Максимальная сумма, которую вы можете заработать —', i)
